1b66252183e2f331b5f916b27b86ddd7804a66b3
MahmoudBashir <mahmoudbashir830@gmail.com>
2022-05-11 18:03:10 +0200

    Mahmoud commit, work on Add shipment summary Design ,,,

--- app/src/main/java/com/ultimate/globalangles/ui/fragment/addShipment/AddShipmentFragmentViewModel.java
+++ app/src/main/java/com/ultimate/globalangles/ui/fragment/addShipment/AddShipmentFragmentViewModel.java
@@ -8,9 +8,7 @@ import com.ultimate.globalangles.ui.base.BaseViewModel;
 
 import javax.inject.Inject;
 
-import dagger.hilt.android.lifecycle.HiltViewModel;
 
-@HiltViewModel
 public class AddShipmentFragmentViewModel extends BaseViewModel {
 
     @Inject
