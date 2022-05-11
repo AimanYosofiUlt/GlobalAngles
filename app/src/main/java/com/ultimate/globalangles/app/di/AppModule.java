73c580e99505507cca018b76736b44ae276e008d
MahmoudBashir <mahmoudbashir830@gmail.com>
2022-05-11 09:18:55 +0200

    Mahmoud commit

--- app/src/main/java/com/ultimate/globalangles/app/di/AppModule.java
+++ app/src/main/java/com/ultimate/globalangles/app/di/AppModule.java
@@ -19,13 +19,14 @@ import dagger.Provides;
 import dagger.hilt.InstallIn;
 import dagger.hilt.android.components.ActivityComponent;
 import dagger.hilt.android.scopes.ActivityScoped;
+import dagger.hilt.components.SingletonComponent;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
-@InstallIn(ActivityComponent.class)
+@InstallIn(SingletonComponent.class)
 public abstract class AppModule {
 
     @Qualifier
@@ -42,7 +43,7 @@ public abstract class AppModule {
     @UltimateApiBaseLink
     @Provides
     public static String provideBaseUrl() {
-        return "https://samir.ultimate-demos.com/wp-json/app/v1/";
+        return "";
     }
 
 
@@ -81,6 +82,7 @@ public abstract class AppModule {
     public static AppDatabase provideAppDatabase(Application application) {
         return AppDatabase.getInstance(application);
     }
+
 
     @ActivityScoped
     @Provides
--- app/src/main/java/com/ultimate/globalangles/ui/activity/main/MainActivity.java
+++ app/src/main/java/com/ultimate/globalangles/ui/activity/main/MainActivity.java
@@ -3,13 +3,31 @@ package com.ultimate.globalangles.ui.activity.main;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.ultimate.globalangles.R;
+import com.ultimate.globalangles.ui.login.LoginViewModel;
 
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
+
+    @Inject
+    LoginViewModel viewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewModel.getListData();
+        viewModel.mlist.observe(this,mlist -> {
+            if (mlist != null){
+                Toast.makeText(this, "wignsidfn", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
