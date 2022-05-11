ff4abc83a0975be8ecf4204ec7cba0e99a8329ed
Ultimate <aimanyosofiult>
2022-05-11 09:52:47 +0200

    Commmit Aiman 1

--- app/src/main/java/com/ultimate/globalangles/utilities/LayoutUtil.java
+++ app/src/main/java/com/ultimate/globalangles/utilities/LayoutUtil.java
@@ -1,15 +1,10 @@
 package com.ultimate.globalangles.utilities;
 
-import android.app.AlertDialog;
-import android.content.Context;
-import android.view.LayoutInflater;
 import android.view.View;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.facebook.shimmer.ShimmerFrameLayout;
-import com.ultimate.ecommerce.R;
-import com.ultimate.ecommerce.databinding.DialogMessageBinding;
 
 public class LayoutUtil {
     public static void showShimmer(ConstraintLayout constraintLayout, ShimmerFrameLayout shimmerFrameLayout) {
@@ -17,30 +12,13 @@ public class LayoutUtil {
         constraintLayout.setAlpha(0);
         shimmerFrameLayout.startShimmer();
     }
-
+    
     public static void hideShimmer(ConstraintLayout constraintLayout, ShimmerFrameLayout shimmerFrameLayout) {
-        shimmerFrameLayout.animate().setDuration(200).alpha(0).withEndAction(new Runnable() {
-            @Override
-            public void run() {
-                shimmerFrameLayout.stopShimmer();
-                shimmerFrameLayout.setVisibility(View.GONE);
-                constraintLayout.setVisibility(View.VISIBLE);
-                constraintLayout.animate().setDuration(1000).alpha(1);
-            }
+        shimmerFrameLayout.animate().setDuration(200).alpha(0).withEndAction(() -> {
+            shimmerFrameLayout.stopShimmer();
+            shimmerFrameLayout.setVisibility(View.GONE);
+            constraintLayout.setVisibility(View.VISIBLE);
+            constraintLayout.animate().setDuration(1000).alpha(1);
         });
-    }
-
-    public static void showMassageDialog(Context context, String title, String message) {
-        View view = LayoutInflater.from(context).inflate(R.layout.dialog_message, null, false);
-        DialogMessageBinding bd  = DialogMessageBinding.bind(view);
-        bd.title.titleTV.setText(title);
-        bd.messageTV.setText(message);
-
-        AlertDialog dialog = new AlertDialog.Builder(context)
-                .setView(view)
-                .create();
-        dialog.show();
-
-        bd.doneBtn.btnBody.setOnClickListener(view1 -> dialog.dismiss());
     }
 }
