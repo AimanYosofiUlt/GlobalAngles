d0a5fca9db6a87679e51df511338e7b64dcf0436
MahmoudBashir <mahmoudbashir830@gmail.com>
2022-05-12 13:17:12 +0200

    Mahmoud commit, work on MenuFragment Design , modify on NavComponent and work on BottomNavigationView . also adding some required drawables.. ,,,

--- app/src/main/java/com/ultimate/globalangles/ui/fragment/login/LoginFragment.java
+++ app/src/main/java/com/ultimate/globalangles/ui/fragment/login/LoginFragment.java
@@ -4,11 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.lifecycle.Observer;
+import androidx.navigation.Navigation;
 
 import com.ultimate.globalangles.databinding.FragmentLoginBinding;
+import com.ultimate.globalangles.repository.server.responses.base.ResponseState;
 import com.ultimate.globalangles.ui.base.BaseFragment;
+import com.ultimate.globalangles.utilities.state.OnValidateListener;
+import com.ultimate.globalangles.utilities.state.StateUtil;
 
 import javax.annotation.Nullable;
 
@@ -22,6 +28,8 @@ public class LoginFragment extends BaseFragment<LoginFragmentViewModel> {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         bd = FragmentLoginBinding.inflate(getLayoutInflater());
+
+
         return bd.getRoot();
     }
 
@@ -31,16 +39,31 @@ public class LoginFragment extends BaseFragment<LoginFragmentViewModel> {
         bd.loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Navigation.findNavController(bd.getRoot()).navigate(LoginFragmentDirections.actionLoginFragmentToHomeMainFragment());
+
+
                 String email = bd.email.getText().toString();
                 String password = bd.password.getText().toString();
-                viewModel.validateLogin(requireContext(),email,password);
+                if (!viewModel.validateLogin(requireContext(),email,password).isEmpty()){
+                    Toast.makeText(requireContext(), "some fields empty", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
 
     @Override
     public void initObservers() {
-
+        viewModel.loginResponseStateMDL.observe(this, new Observer<ResponseState>() {
+            @Override
+            public void onChanged(ResponseState responseState) {
+                if (responseState.isSuccessful()){
+                    //do navigate
+                }else {
+                    //show error message
+                    Toast.makeText(requireContext(), ""+responseState.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     @Override
